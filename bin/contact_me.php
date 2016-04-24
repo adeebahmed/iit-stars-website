280a56049870096e660c0319255ac576b024ea20
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2016-04-24 15:15:38 -0500

    Added new pages; Updating contact; Added full view on index;

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -1,9 +1,8 @@
 <?php
 // check if fields passed are empty
 if(empty($_POST['name'])  		||
-   empty($_POST['phone']) 		||
    empty($_POST['email']) 		||
-   empty($_POST['message'])	||
+   empty($_POST['comments'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
@@ -11,15 +10,14 @@ if(empty($_POST['name'])  		||
    }
 	
 $name = $_POST['name'];
-$phone = $_POST['phone'];
 $email_address = $_POST['email'];
-$message = $_POST['message'];
+$message = $_POST['comments'];
 	
 // create email body and send it	
-$to = 'your-email@your-domain.com'; // PUT YOUR EMAIL ADDRESS HERE
+$to = 'adeeb.ahmed1995@gmail.com'; // PUT YOUR EMAIL ADDRESS HERE
 $email_subject = "Modern Business Contact Form:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
-$email_body = "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@your-domain.com\n";
+$email_body = "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: $email_address\n";
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
